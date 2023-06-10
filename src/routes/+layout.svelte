961e30645838ede6fcf51c17f54377e26b8d9fe3
te <te1@users.noreply.github.com>
2023-06-10 17:20:03 +0200

    add @vercel/analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
+  import { dev } from '$app/environment';
+  import { inject } from '@vercel/analytics';
+
   import '../app.postcss';
+
+  inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <slot />
