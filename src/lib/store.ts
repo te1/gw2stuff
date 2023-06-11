1c9cb9ff1de1253651820132b7820a4bac2ec5a5
te <te1@users.noreply.github.com>
2023-06-11 18:24:30 +0200

    don't call server for api key validation if there is no key

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -6,13 +6,21 @@ export const apiKey = persisted<string>('apiKey', '');
 export const apiKeyValid = writable<boolean | null>(null);
 
 export async function updateValidateApiKey() {
+  const apiKeyValue = get(apiKey);
+
+  if (!apiKeyValue) {
+    apiKeyValid.set(false);
+
+    return;
+  }
+
   const res = await fetch('/api/validateApiKey', {
     method: 'POST',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ apiKey: get(apiKey) }),
+    body: JSON.stringify({ apiKey: apiKeyValue }),
   });
 
   const { valid } = await res.json();
