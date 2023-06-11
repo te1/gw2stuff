844190e4773c68b9794ed5fa9ea25d29db72614c
te <te1@users.noreply.github.com>
2023-06-11 17:55:11 +0200

    header links to home page

--- src/lib/components/AppHeader.svelte
+++ src/lib/components/AppHeader.svelte
@@ -3,10 +3,12 @@
 </script>
 
 <header class="mb-8 flex items-center justify-between border-b border-primary pb-4 pt-8">
-  <h1 class="text-2xl">
-    <span class="font-bold">Guild Wars 2</span>
-    <span class="font-light">Where Is My Stuff?</span>
-  </h1>
+  <a href="/">
+    <h1 class="text-2xl">
+      <span class="font-bold">Guild Wars 2</span>
+      <span class="font-light">Where Is My Stuff?</span>
+    </h1>
+  </a>
 
   <LightSwitch />
 </header>
