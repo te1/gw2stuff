c8b2797f10c498b7a2fa8104a62e2ce1c9618e09
te <te1@users.noreply.github.com>
2023-06-11 18:18:48 +0200

    validatae api key on page load

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,6 +1,21 @@
-import { writable } from 'svelte/store';
+import { get, writable } from 'svelte/store';
 import { persisted } from 'svelte-local-storage-store';
 
 export const apiKey = persisted<string>('apiKey', '');
 
-export const apiKeyValid = writable<boolean>(false);
+export const apiKeyValid = writable<boolean | null>(null);
+
+export async function updateValidateApiKey() {
+  const res = await fetch('/api/validateApiKey', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ apiKey: get(apiKey) }),
+  });
+
+  const { valid } = await res.json();
+
+  apiKeyValid.set(valid);
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,25 @@
 <script type="ts">
-  import { apiKeyValid } from '$lib/store';
+  import { onMount } from 'svelte';
+  import { apiKeyValid, updateValidateApiKey } from '$lib/store';
+
+  onMount(() => {
+    updateValidateApiKey();
+  });
 </script>
 
-<p class="mb-4">
-  Find gear and other items anywhere on your
-  <a href="https://www.guildwars2.com" target="_blank">Guild Wars 2</a>
-  account. It will search your bank, inventory and equipment tabs (even inactive ones!) on all your characters.
-  This can come in handy if you are looking for your account bound
-  <span class="text-ascended">ascended</span> gear or unlimited gathering tools.
-</p>
+<section class="mb-4">
+  <p>
+    Find gear and other items anywhere on your
+    <a href="https://www.guildwars2.com" target="_blank">Guild Wars 2</a>
+    account. It will search your bank, inventory and equipment tabs (even inactive ones!) on all your
+    characters. This can come in handy if you are looking for your account bound
+    <span class="text-ascended">ascended</span> gear or unlimited gathering tools.
+  </p>
+</section>
 
-{#if $apiKeyValid}
+{#if $apiKeyValid == null}
+  <p>Checking API key...</p>
+{:else if $apiKeyValid}
   <p>Your API key is valid. You can go find your stuff now.</p>
 {:else}
   <p>
