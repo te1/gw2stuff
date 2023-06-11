b5681a83453381a3c1645c14bb75f5b13ae617ae
te <te1@users.noreply.github.com>
2023-06-11 17:34:09 +0200

    add item rarity colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,6 +51,14 @@ const config = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        junk: '#aaaaaa',
+        basic: '#000000',
+        fine: '#62a4da',
+        masterwork: '#1a9306',
+        rare: '#fcd00b',
+        exotic: '#ffa405',
+        ascended: '#fb3e8d',
+        legendary: '#4c139d',
       },
 
       borderRadius: {
