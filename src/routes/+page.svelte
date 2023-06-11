528578f6adc6e1ce30872b54fe6e0520a52a9cb1
te <te1@users.noreply.github.com>
2023-06-12 00:02:19 +0200

    format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,25 +9,21 @@
 </script>
 
 <section class="mb-4">
-  <p>
-    Find gear and other items anywhere on your
-    <a href="https://www.guildwars2.com" target="_blank">Guild Wars 2</a>
-    account. It will search your bank, inventory and equipment tabs (even inactive ones!) on all your
-    characters. This can come in handy if you are looking for your account bound
-    <span class="text-ascended">ascended</span> gear or unlimited gathering tools.
-  </p>
+  Find gear and other items anywhere on your
+  <a href="https://www.guildwars2.com" target="_blank">Guild Wars 2</a>
+  account. It will search your bank, inventory and equipment tabs (even inactive ones!) on all your characters.
+  This can come in handy if you are looking for your account bound
+  <span class="text-ascended">ascended</span> gear or unlimited gathering tools.
 </section>
 
 {#key $apiKeyValid}
   <section in:fade|local>
     {#if $apiKeyValid == null}
-      <p>Checking API key...</p>
+      Checking API key...
     {:else if $apiKeyValid}
-      <p>Your API key is valid. You can go find your stuff now.</p>
+      Your API key is valid. You can go find your stuff now.
     {:else}
-      <p>
-        You need to <a href="/settings" class="link">setup an API key</a> before you can use this tool.
-      </p>
+      You need to <a href="/settings" class="link">setup an API key</a> before you can use this tool.
     {/if}
   </section>
 {/key}
