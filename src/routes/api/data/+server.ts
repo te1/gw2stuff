13c7ecec9db4018d71c50cdb2c80960f00cb83bd
te <te1@users.noreply.github.com>
2023-06-13 22:31:13 +0200

    use data collector

--- src/routes/api/data/+server.ts
+++ src/routes/api/data/+server.ts
@@ -1,9 +1,11 @@
 import { json } from '@sveltejs/kit';
 import { makeGw2Api } from '$lib/gw2/api.server';
+import { Gw2DataCollector } from '$lib/gw2/collect.server';
 
 export async function POST({ request, fetch }) {
   const api = await makeGw2Api(request, fetch);
-  const data = await api.collect();
+  const collector = new Gw2DataCollector(api);
+  const data = await collector.collect();
 
   return json(data);
 }
